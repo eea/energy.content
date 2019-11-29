4eba57a072be9e0cc0bbb0c762ad1279d0238583
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-29 17:07:36 +0200

    Added resource type as querystring criteria

--- src/energy/content/upgrades/to1002.py
+++ src/energy/content/upgrades/to1002.py
@@ -4,7 +4,7 @@ def run_upgrade(setup_context):
 
     setup_context.runImportStepFromProfile(
         "profile-energy.content:default",
-        "catalog",
+        "plone.app.registry",
         run_dependencies=False,
         purge_old=False,
     )
