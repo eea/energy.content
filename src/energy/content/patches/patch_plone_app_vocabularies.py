8becccdec79090bcab46abc79b83c79fdfa33b3b
alecghica <alecghica@gmail.com>
2021-06-10 19:44:33 +0000

    patched metadatafields vocabulary from plone.app.vocabularies, refs #134167

---
+++ src/energy/content/patches/patch_plone_app_vocabularies.py
@@ -0,0 +1,34 @@
+""" Patch for plone.app.vocabularies.metadatafields
+"""
+
+_FIELD_LABEL_ENERGY = {
+    'CreationDate': _('Created on'),
+    'Creator': _('Creator'),
+    'Description': _('Description'),
+    'EffectiveDate': _('Publication date'),
+    'end': _('End Date'),
+    'exclude_from_nav': _('Excluded from navigation'),
+    'ExpirationDate': _('Expiration date'),
+    'getObjSize': _('Object Size'),
+    'id': _('ID'),
+    'is_folderish': _('Folder'),
+    'last_comment_date': _('Last comment date'),
+    'location': _('Location'),
+    'ModificationDate': _('Last modified'),
+    'review_state': _('Review state'),
+    'start': _('Start Date'),
+    'Subject': _('Tags'),
+    'Type': _('Type'),
+    'total_comments': _('Total comments'),
+    'mime_type': _('MIME type'),
+    'resource_type': _('Resource Type'),
+    'meta_topics': _('Topics'),
+}
+
+def updated_metadatafields(self, context):
+    cat = getToolByName(context, 'portal_catalog')
+    items = [
+        SimpleTerm(column, column, _FIELD_LABEL_ENERGY[column] if column in _FIELD_LABEL_ENERGY else _(column))
+        for column in cat.schema()
+    ]
+    return SimpleVocabulary(items)
